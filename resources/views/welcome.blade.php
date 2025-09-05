ef05df6280ade3a6964eddd355715921282615ee
Mohammed Sohail <hey@msohail.dev>
2025-09-05 13:45:22 +0300

    Upgrade to tailwind v4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,7 +53,7 @@
 
                     <main class="mt-6">
                         <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
-                            <div class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
+                            <div class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/5 transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
                                 <h2 class="text-xl font-semibold text-black dark:text-white">Welcome to the Identity API</h2>
                                 <p class="text-sm/relaxed">
                                     This Laravel application provides a secure, flexible API for contextual identity management.
